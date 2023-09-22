dd59fc0f38ba50b938861e22fbad18e3dca5a333
Stanislav_Starostin <stas.starostin19@gmail.com>
2023-09-22 12:44:33 +0300

    556666

--- hw.py
+++ hw.py
@@ -8,4 +8,40 @@ class Car:
         self.mileage = mileage
         self.engine_on = False
 
+    def start_engine(self):
+        if not self.engine_on and self.reserve > 0:
+            self.engine_on = True
+            return "Двигатель запущен"
+        return "Двигатель уже был остановлен"
+
+    def stop_engine(self):
+        if self.engine_on:
+            self.engine_on = False
+            return "Двигатель остановлен."
+        return "Двигатель уже был остановлен."
+
+
+    def drive(self, distance):
+        if not self.engine_on:
+            return "Двигатель не запущен."
+        if self.reserve / self.consumption * 100 < distance:
+            return "Малый запас топлива."
+        self.mileage += distance
+        self.reserve -= distance / 100 * self.consumption
+        return f"Проехали {distance} км. Остаток топлива: {self.reserve} л."
+
+    def refuel(self):
+        self.reserve = self.tank_volume
+
+    def get_mileage(self):
+        return self.mileage
+
+    def get_reserve(self):
+        return self.reserve
+
+car_1 = Car(color="black", consumption=10, tank_volume=55)
+print(car_1.start_engine())
+print(car_1.drive(100))
+print(car_1.stop_engine())
+print(car_1.drive(100))
 
