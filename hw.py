07299e65abd59ac289f293ad51ad402bc4df2e1d
Stanislav_Starostin <stas.starostin19@gmail.com>
2023-09-24 11:14:40 +0300

    77777

--- hw.py
+++ hw.py
@@ -1,7 +1,7 @@
 class Car:
 
     def __init__(self, color, consumption, tank_volume, mileage = 0):
-        self.color = color
+        self.__color = color
         self.consumption = consumption
         self.tank_volume = tank_volume
         self.reserve = tank_volume
